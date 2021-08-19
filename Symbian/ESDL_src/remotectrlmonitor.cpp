6d8ac1cbb8a9e95f863c9589955f83f603c943a3
Fiodar Stryzhniou <fedor_qd@mail.ru>
2021-08-19 13:03:41 +0300

    remotectrlmonitor.cpp/h: Code style fix

--- Symbian/ESDL_src/remotectrlmonitor.cpp
+++ Symbian/ESDL_src/remotectrlmonitor.cpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "remotectrlmonitor.h"
 
 #include "SDL_keysym.h"
@@ -8,16 +10,16 @@ extern "C"
 	#include "SDL_events_c.h"
 	#include "SDL_sysvideo.h"
 	}
-#include <stdio.h>
 #define REMCONKEY1 SDLK_PAGEUP
 #define REMCONKEY2 SDLK_PAGEDOWN
-#define REMCONKEY3 SDLK_INSERT 
+#define REMCONKEY3 SDLK_INSERT
 #define REMCONKEY4 SDLK_LALT
+
 void IncreaseVolume();
 void DecreaseVolume();
 extern SDL_VideoDevice * current_video;
 //void EPOC_GenerateKeyEvent(SDL_VideoDevice *_this,int aScanCode,int aIsDown, TBool aShiftOn);
-CRemConEventTimer* CRemConEventTimer::NewL(TInt aKeyCode, CRemoteCtrlEventMonitor* aMonitor) 
+CRemConEventTimer* CRemConEventTimer::NewL(TInt aKeyCode, CRemoteCtrlEventMonitor* aMonitor)
     {
     CRemConEventTimer* self = new (ELeave) CRemConEventTimer(aKeyCode, aMonitor);
     CleanupStack::PushL(self);
@@ -25,45 +27,44 @@ CRemConEventTimer* CRemConEventTimer::NewL(TInt aKeyCode, CRemoteCtrlEventMonito
     CleanupStack::Pop();
     return self;
     }
-CRemConEventTimer::~CRemConEventTimer() 
+
+CRemConEventTimer::~CRemConEventTimer()
     {
     Cancel();
     iTimer.Close();
     }
-    
-void CRemConEventTimer::DoCancel() 
+
+void CRemConEventTimer::DoCancel()
     {
     iTimer.Cancel();
     }
 
-    
-void CRemConEventTimer::RunL() 
+void CRemConEventTimer::RunL()
     {
     if (iStatus.Int() == KErrCancel)
         {
         return;
-        } 
+        }
     //Key_Event(iKeyCode, qfalse, Sys_Milliseconds());
-    iMonitor->GenerateEvent(iKeyCode, 0);    
+    iMonitor->GenerateEvent(iKeyCode, 0);
     }
-    
-void CRemConEventTimer::IssueRequest(TInt aTimeOutInMillis) 
+
+void CRemConEventTimer::IssueRequest(TInt aTimeOutInMillis)
     {
 	iStatus = KRequestPending;
-	iTimer.After(iStatus, aTimeOutInMillis*1000);    
+	iTimer.After(iStatus, aTimeOutInMillis*1000);
     SetActive();
     }
 
-CRemConEventTimer::CRemConEventTimer(TInt aKeyCode, CRemoteCtrlEventMonitor* aMonitor) : CActive(EPriorityNormal), iKeyCode(aKeyCode), iMonitor(aMonitor) 
+CRemConEventTimer::CRemConEventTimer(TInt aKeyCode, CRemoteCtrlEventMonitor* aMonitor) : CActive(EPriorityNormal), iKeyCode(aKeyCode), iMonitor(aMonitor)
     {
     CActiveScheduler::Add(this);
     }
 
-void CRemConEventTimer::ConstructL() 
+void CRemConEventTimer::ConstructL()
     {
     User::LeaveIfError(iTimer.CreateLocal());
     }
-
 
 int CRemoteCtrlEventMonitor::MapRemoteCtrlKey(TRemConCoreApiOperationId aOp)
     {
@@ -71,20 +72,17 @@ int CRemoteCtrlEventMonitor::MapRemoteCtrlKey(TRemConCoreApiOperationId aOp)
         {
         case ERemConCoreApiVolumeUp:
             return REMCONKEY1;
-        case ERemConCoreApiVolumeDown:                    
+        case ERemConCoreApiVolumeDown:
             return REMCONKEY2;
         default: return 0;
         }
     }
 
+CRemoteCtrlEventMonitor::CRemoteCtrlEventMonitor() {}
 
-CRemoteCtrlEventMonitor::CRemoteCtrlEventMonitor() 
-    {
-    }
-    
 CRemoteCtrlEventMonitor::~CRemoteCtrlEventMonitor()
     {
-    delete iInterfaceSelector;    
+    delete iInterfaceSelector;
     delete iCancelTimers[0];
     delete iCancelTimers[1];
     }
@@ -106,37 +104,36 @@ void CRemoteCtrlEventMonitor::ConstructL()
     iCancelTimers[0] = CRemConEventTimer::NewL(REMCONKEY1, this);
     iCancelTimers[1] = CRemConEventTimer::NewL(REMCONKEY2, this);
     }
-    
+
 void CRemoteCtrlEventMonitor::GenerateEvent(int keycode, int pressed)
-	{
+{
 	if(current_video->hidden->iControlKeyDown)
-		{
+	{
 		if(pressed)
-			{
-			if (keycode == REMCONKEY1) 
-				IncreaseVolume(); 
-			else 
-				DecreaseVolume();
-			}
-		}
-	else if(current_video->hidden->iHasMouseOrTouch)
 		{
+			if (keycode == REMCONKEY1)
+				IncreaseVolume();
+			else
+				DecreaseVolume();
+		}
+	}
+	else if(current_video->hidden->iHasMouseOrTouch)
+	{
 		int sdlPressed = pressed?SDL_PRESSED:SDL_RELEASED;
 		int sdlMouseButton = (keycode == REMCONKEY1)?SDL_BUTTON_LEFT:SDL_BUTTON_RIGHT;
-		SDL_PrivateMouseButton(sdlPressed, sdlMouseButton, 0, 0);		
-		}
-
+		SDL_PrivateMouseButton(sdlPressed, sdlMouseButton, 0, 0);
+	}
 	else
-		{				
-		SDL_keysym keysym;		
-		SDLMod modState = SDL_GetModState();		
+	{
+		SDL_keysym keysym;
+		SDLMod modState = SDL_GetModState();
 		keysym.scancode = keycode;
 		keysym.sym = (SDLKey) keycode;
 		keysym.mod = modState;
-		keysym.unicode = keycode;									
-		SDL_PrivateKeyboard(pressed?SDL_PRESSED:SDL_RELEASED, &keysym);		
-		}
+		keysym.unicode = keycode;
+		SDL_PrivateKeyboard(pressed?SDL_PRESSED:SDL_RELEASED, &keysym);
 	}
+}
 
 void CRemoteCtrlEventMonitor::MrccatoCommand(TRemConCoreApiOperationId aOperationId, TRemConCoreApiButtonAction aButtonAct)
     {
@@ -144,7 +141,7 @@ void CRemoteCtrlEventMonitor::MrccatoCommand(TRemConCoreApiOperationId aOperatio
     int remconcode = MapRemoteCtrlKey(aOperationId);
     switch (aButtonAct)
         {
-        case ERemConCoreApiButtonPress:        	
+        case ERemConCoreApiButtonPress:
             if (remconcode == REMCONKEY1)
                 {
                 iCancelTimers[0]->Cancel();
@@ -154,13 +151,13 @@ void CRemoteCtrlEventMonitor::MrccatoCommand(TRemConCoreApiOperationId aOperatio
                 iCancelTimers[1]->Cancel();
                 }
             break;
-        case ERemConCoreApiButtonRelease:        	
+        case ERemConCoreApiButtonRelease:
             //Key_Event(remconcode, qfalse, Sys_Milliseconds());
-        	GenerateEvent(remconcode, 0);            
+        	GenerateEvent(remconcode, 0);
             break;
-        case ERemConCoreApiButtonClick:        	
+        case ERemConCoreApiButtonClick:
             if (remconcode == REMCONKEY1)
-                {          
+                {
                 if (iCancelTimers[0]->IsActive())
                     {
                     //Key_Event(remconcode, qfalse, Sys_Milliseconds());
@@ -185,7 +182,7 @@ void CRemoteCtrlEventMonitor::MrccatoCommand(TRemConCoreApiOperationId aOperatio
         default:
             break;
         }
-    
+
     switch(aOperationId)
     	{
     	case ERemConCoreApiPlay:
@@ -203,36 +200,35 @@ void CRemoteCtrlEventMonitor::MrccatoCommand(TRemConCoreApiOperationId aOperatio
       default:
         break;
     	}
-            
+
     }
 
-
-/** 
-A 'play' command has been received. 
+/**
+A 'play' command has been received.
 @param aSpeed The playback speed.
 @param aButtonAct The button action associated with the command.
 */
- void CRemoteCtrlEventMonitor::MrccatoPlay(TRemConCoreApiPlaybackSpeed aSpeed, 
+ void CRemoteCtrlEventMonitor::MrccatoPlay(TRemConCoreApiPlaybackSpeed aSpeed,
 	TRemConCoreApiButtonAction aButtonAct)
 	 {
-	 
+
 	 }
 
 /**
 A 'tune function' command has been received.
-@param aTwoPart If EFalse, only aMajorChannel is to be used. Otherwise, 
+@param aTwoPart If EFalse, only aMajorChannel is to be used. Otherwise,
 both aMajorChannel and aMinorChannel are to be used.
 @param aMajorChannel The major channel number.
 @param aMinorChannel The minor channel number.
 @param aButtonAct The button action associated with the command.
 */
- void CRemoteCtrlEventMonitor::MrccatoTuneFunction(TBool aTwoPart, 
-	TUint aMajorChannel, 
+ void CRemoteCtrlEventMonitor::MrccatoTuneFunction(TBool aTwoPart,
+	TUint aMajorChannel,
 	TUint aMinorChannel,
 	TRemConCoreApiButtonAction aButtonAct)
 	 {	 }
 
-/** 
+/**
 A 'select disk function' has been received.
 @param aDisk The disk.
 @param aButtonAct The button action associated with the command.
@@ -241,7 +237,7 @@ A 'select disk function' has been received.
 	TRemConCoreApiButtonAction aButtonAct)
 	 {}
 
-/** 
+/**
 A 'select AV input function' has been received.
 @param aAvInputSignalNumber The AV input.
 @param aButtonAct The button action associated with the command.
@@ -250,7 +246,7 @@ A 'select AV input function' has been received.
 	TRemConCoreApiButtonAction aButtonAct){
  }
 
-/** 
+/**
 A 'select audio input function' has been received.
 @param aAudioInputSignalNumber The audio input.
 @param aButtonAct The button action associated with the command.
--- Symbian/ESDL_src/remotectrlmonitor.h
+++ Symbian/ESDL_src/remotectrlmonitor.h
@@ -34,36 +34,36 @@ class CRemoteCtrlEventMonitor : public CBase, public MRemConCoreApiTargetObserve
         ~CRemoteCtrlEventMonitor();
         void MrccatoCommand(TRemConCoreApiOperationId aOperationId,
                             TRemConCoreApiButtonAction aButtonAct);
-    	/** 
-    	A 'play' command has been received. 
+    	/**
+    	A 'play' command has been received.
     	@param aSpeed The playback speed.
     	@param aButtonAct The button action associated with the command.
     	*/
-    	virtual void MrccatoPlay(TRemConCoreApiPlaybackSpeed aSpeed, 
+    	virtual void MrccatoPlay(TRemConCoreApiPlaybackSpeed aSpeed,
     		TRemConCoreApiButtonAction aButtonAct);
 
     	/**
     	A 'tune function' command has been received.
-    	@param aTwoPart If EFalse, only aMajorChannel is to be used. Otherwise, 
+    	@param aTwoPart If EFalse, only aMajorChannel is to be used. Otherwise,
     	both aMajorChannel and aMinorChannel are to be used.
     	@param aMajorChannel The major channel number.
     	@param aMinorChannel The minor channel number.
     	@param aButtonAct The button action associated with the command.
     	*/
-    	virtual void MrccatoTuneFunction(TBool aTwoPart, 
-    		TUint aMajorChannel, 
+    	virtual void MrccatoTuneFunction(TBool aTwoPart,
+    		TUint aMajorChannel,
     		TUint aMinorChannel,
     		TRemConCoreApiButtonAction aButtonAct);
 
-    	/** 
+    	/**
     	A 'select disk function' has been received.
     	@param aDisk The disk.
     	@param aButtonAct The button action associated with the command.
     	*/
     	virtual void MrccatoSelectDiskFunction(TUint aDisk,
     		TRemConCoreApiButtonAction aButtonAct);
-    	
-    	/** 
+
+    	/**
     	A 'select AV input function' has been received.
     	@param aAvInputSignalNumber The AV input.
     	@param aButtonAct The button action associated with the command.
@@ -71,7 +71,7 @@ class CRemoteCtrlEventMonitor : public CBase, public MRemConCoreApiTargetObserve
     	virtual void MrccatoSelectAvInputFunction(TUint8 aAvInputSignalNumber,
     		TRemConCoreApiButtonAction aButtonAct);
 
-    	/** 
+    	/**
     	A 'select audio input function' has been received.
     	@param aAudioInputSignalNumber The audio input.
     	@param aButtonAct The button action associated with the command.
@@ -81,8 +81,8 @@ class CRemoteCtrlEventMonitor : public CBase, public MRemConCoreApiTargetObserve
         void GenerateEvent(int keycode, int pressed);
     protected:
         CRemoteCtrlEventMonitor();
-        void ConstructL();      
-        int MapRemoteCtrlKey(TRemConCoreApiOperationId aOp);        
+        void ConstructL();
+        int MapRemoteCtrlKey(TRemConCoreApiOperationId aOp);
     private:
         CRemConInterfaceSelector* iInterfaceSelector;
         CRemConCoreApiTarget*     iCoreTarget;
